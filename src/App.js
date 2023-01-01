0dc226d989889cb774c03b399e0beaf084ba3d99
Christoph Poellmann <christoph.poellmann@bbs-buchholz.de>
2023-01-01 17:05:30 +0100

    Update BrowserRouter implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from "react";
 import Header from "./components/Header";
 import Play from "./components/Play";
-import Game from "./components/Game";
+import Match from "./components/Match";
 import Footer from "./components/Footer";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div className="container">
         <Header />
-        <Switch>
-          <Route exact path="/">
-            <Play />
-          </Route>
-          <Route path="/game">
-            <Game />
-          </Route>
-        </Switch>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Play />} />
+            <Route path="game" element={<Match />} />
+          </Routes>
+        </BrowserRouter>
       </div>
       <Footer />
     </>
