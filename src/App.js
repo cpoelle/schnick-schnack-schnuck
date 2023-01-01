f886603eef4bde597e463b18fd56aaa2c45cde0c
Christoph Poellmann <christoph.poellmann@bbs-buchholz.de>
2023-01-01 16:42:00 +0100

    Add components and basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import Header from "./components/Header";
+import Play from "./components/Play";
+import Game from "./components/Game";
+import Footer from "./components/Footer";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Play />
+          </Route>
+          <Route path="/game">
+            <Game />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </>
   );
 }
 
